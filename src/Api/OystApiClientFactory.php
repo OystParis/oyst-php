d1b9dbb741ba4b66e2dc9a4143aedbc096086b97
Mehdi Chaouch <mehdichaouch@users.noreply.github.com>
2017-08-22 09:57:10 +0200

    Add version method #33 (#52)

--- src/Api/OystApiClientFactory.php
+++ src/Api/OystApiClientFactory.php
@@ -17,6 +17,12 @@ use Symfony\Component\Yaml\Parser;
  */
 class OystApiClientFactory
 {
+    private static $version = array(
+        'major' => '1',
+        'minor' => '6',
+        'patch' => '0',
+    );
+
     const ENTITY_CATALOG = 'catalog';
     const ENTITY_ORDER = 'order';
     const ENTITY_PAYMENT = 'payment';
@@ -24,6 +30,18 @@ class OystApiClientFactory
 
     const ENV_PROD = 'prod';
     const ENV_PREPROD = 'preprod';
+
+    /**
+     * Gets the current version string
+     *
+     * @return string
+     */
+    public static function getVersion()
+    {
+        $ver = self::$version;
+
+        return trim("{$ver['major']}.{$ver['minor']}.{$ver['patch']}");
+    }
 
     /**
      * Returns the right API for the entityName passed in the parameters
